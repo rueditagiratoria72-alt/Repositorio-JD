490fbd4f037ff1b71af86301cd16a45a385702e6
JuansDi15 <rueditagiratoria72@gmail.com>
2026-06-30 14:44:57 -0500

    Se agregó una clase abstracta para vehículos, y se cresron clases que heredaron sus atributos

--- import os.py
+++ import os.py
@@ -1,9 +1,32 @@
 import os
 from abc import ABC
-
-class Auto(ABC):
-    def __init__(self, marca, modelo, año):
+class vehículo(ABC):
+    def __init__(self, marca, modelo,año):
         self.marca = marca
         self.modelo = modelo
         self.año = año
+
+    def mostrar_info(self):
+        return f"Marca: {self.marca}, Modelo: {self.modelo}, Año: {self.año}"
+
+class automóvil(vehículo):
+    def __init__(self, marca, modelo, año):
+        super().__init__(marca, modelo, año)
+
+    def mostrar_info(self):
+        return super().mostrar_info()
+
+class motocicleta(vehículo):
+    def __init__(self, marca, modelo, año):
+        super().__init__(marca, modelo, año)
+
+    def mostrar_info(self):
+        return super().mostrar_info()
+
+class bicicleta(vehículo):
+    def __init__(self, marca, modelo, año):
+        super().__init__(marca, modelo, año)
+
+    def mostrar_info(self):
+        return super().mostrar_info()
         
